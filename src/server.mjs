5b90bcab8b3b8e6d5d741efbba8a7bbbd5fdc001
Wasim Zaman <wasimxaman13@gmail.com>
2025-04-20 09:50:26 +0500

    refactor: remove unused path and url imports, simplify uploads static path

--- src/server.mjs
+++ src/server.mjs
@@ -1,6 +1,4 @@
 import dotenv from "dotenv";
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
 
 import bodyParser from "body-parser";
 import express from "express";
@@ -12,8 +10,6 @@ import routes from "./routes.mjs";
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 const app = express();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 // Middlewares
 app.use(bodyParser.json());
@@ -21,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors);
 
 app.use("/public", express.static("public"));
-app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
+app.use("/uploads", express.static("uploads"));
 
 app.use("/api/v1", routes);
 
